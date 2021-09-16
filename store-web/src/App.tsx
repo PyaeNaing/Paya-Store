55e4dfa45ec4373a6f17d198244ce55470760004
Pyae Naing <pnaing@mail.sfsu.edu>
2021-09-15 22:49:35 -0500

    working on login page

---
+++ store-web/src/App.tsx
@@ -0,0 +1,11 @@
+import logo from "./logo.svg";
+import React from "react";
+import Login from './Components/Login/Login'
+
+function App() {
+  return <React.Fragment>
+    <Login/>
+  </React.Fragment>;
+}
+
+export default App;
---
+++ store-web/src/Components/Login/Login.tsx
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+
+export interface IAppProps {
+}
+
+export default function App (props: IAppProps) {
+    
+    const [logIn, setLogIn] = useState('')
+    const [password, setPassword] = useState('')
+    
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
