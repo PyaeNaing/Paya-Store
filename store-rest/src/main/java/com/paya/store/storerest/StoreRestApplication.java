ffe578e0ec58065ea79a62cc0465a501e23919c0
Pyae Naing <pnaing@mail.sfsu.edu>
2021-09-17 18:27:35 -0500

    added jdbc and spring boot

---
+++ store-rest/src/main/java/com/paya/store/storerest/StoreRestApplication.java
@@ -0,0 +1,13 @@
+package com.paya.store.storerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreRestApplication.class, args);
+	}
+
+}
